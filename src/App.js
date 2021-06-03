306f487dec7a97d562bd1f0c2f9d4c52d4c674a3
Jimmy Lin <b00502013@gmail.com>
2021-06-04 01:22:43 +0800

    add a full-height background image with css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const App = () => {
-  return <h1 className="p-5 text-green-500">Hello World</h1>;
+  return (
+    <div className="header h-full">
+      <h1 className="p-5 text-5xl text-gray-500">j3y</h1>
+    </div>
+  );
 };
 
 export default App;
