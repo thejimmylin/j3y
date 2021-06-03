75d7cd55384c0dd32cdd785c8549ac270a84591d
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-06-03 17:02:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# redjango
+My react &amp; django repository template
+
+# Usages
+
+This template provides
+
+1. .gitignore suitable for django & react project
+2. tasks.py for vscode/secrets.json default config which assume you want use:
+    1. black
+    2. flake8
+    3. prettier
+
+    for vscode linter & formatter
+
+# Installations
+
+1. cd ~repos/redjango
+2. python3 -m venv .venv
+3. source .venv/bin/activate
+4. pip install invoke
+5. invoke updateconfig
+6. ... install what you want/ what you need
---
+++ tasks.py
@@ -0,0 +1,59 @@
+import sys
+from pathlib import Path
+
+from invoke import task
+
+from configadmins import JsonConfigAdmin
+
+REPO_ROOT = Path(__file__).parent
+PYTHON_PATH = Path(sys.executable)
+PYTHON_PATH_RELATIVE = Path(sys.executable).relative_to(REPO_ROOT)
+
+VSCODE_SETTINGS_PYTHON = {
+    "python.pythonPath": str(PYTHON_PATH_RELATIVE),
+    "python.linting.enabled": True,
+    "python.linting.pylintEnabled": False,
+    "python.linting.flake8Enabled": True,
+    "python.linting.flake8Args": ["--max-line-length=88"],
+    "python.formatting.provider": "black",
+}
+
+VSCODE_SETTINGS_HTML = {
+    "editor.linkedEditing": True,
+}
+
+VSCODE_SETTINGS_REACT = {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "[javascript]": {"editor.defaultFormatter": "esbenp.prettier-vscode"},
+    "prettier.tabWidth": 2,
+    "prettier.printWidth": 160,
+}
+
+VSCODE_SETTINGS = dict(
+    **VSCODE_SETTINGS_PYTHON,
+    **VSCODE_SETTINGS_HTML,
+    **VSCODE_SETTINGS_REACT,
+)
+
+SECRETS = {
+    "GMAIL_EMAIL_HOST_USER": "",
+    "GMAIL_EMAIL_HOST_PASSWORD": "",
+}
+
+
+@task
+def setdefaultconfig(c):
+    JsonConfigAdmin(
+        path=REPO_ROOT / ".vscode" / "settings.json", options=VSCODE_SETTINGS
+    ).live().setdefault()
+    JsonConfigAdmin(
+        path=REPO_ROOT / "secrets.json", options=SECRETS
+    ).live().setdefault()
+
+
+@task
+def updateconfig(c):
+    JsonConfigAdmin(
+        path=REPO_ROOT / ".vscode" / "settings.json", options=VSCODE_SETTINGS
+    ).live().update()
+    JsonConfigAdmin(path=REPO_ROOT / "secrets.json", options=SECRETS).live().update()
