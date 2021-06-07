d1e12165baec0952e3b6e16bd59ee34c25670469
Jimmy Lin <b00502013@gmail.com>
2021-06-07 10:29:23 +0800

    Update settings.py

--- j3y/settings.py
+++ j3y/settings.py
@@ -114,3 +114,7 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Static
+
+STATIC_ROOT = BASE_DIR / 'static'
