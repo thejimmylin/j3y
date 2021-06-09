ec47bb37f5f70eaf65cdd1488c3d756eb2264bb0
Jimmy Lin <b00502013@gmail.com>
2021-06-10 02:02:06 +0800

    use tailwind customize font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      pretty: ["Libre Caslon Text", "serif"],
+      gorgeous: ["Libre Baskerville", "serif"],
+    },
     extend: {},
   },
   variants: {
