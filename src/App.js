4a8d683042b3367377e9deaf7e3168576651cc7d
Jimmy Lin <b00502013@gmail.com>
2021-06-10 21:40:12 +0800

    npm run build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { useState, useEffect } from "react";
-import Loader from "./components/Loader";
 import Home from "./components/Home";
 
 const App = () => {
-  const [isLoading, setIsLoadig] = useState(true);
-  useEffect(() => {
-    setTimeout(setIsLoadig(false), 8000);
-  }, []);
-  return <>{isLoading ? <Loader /> : <Home />}</>;
+  return <Home />;
 };
 
 export default App;
