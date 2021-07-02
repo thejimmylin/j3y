cc538aebd8060f0aa643f3583d59eedd061fe300
Jimmy Lin <b00502013@gmail.com>
2021-07-03 01:18:37 +0800

    update material

--- src/index/App/HomeView.js
+++ src/index/App/HomeView.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import onewayCompressed from "./HomeView/oneway-192x108-compressed.jpg";
+import onewayCompressed from "./HomeView/wall.jpg";
 import oneway from "./HomeView/wall.jpg";
 import avatarCompressed from "./HomeView/avatar-144x144-compressed.jpg";
 import avatar from "./HomeView/avatar-1440x1440.jpg";
@@ -22,15 +22,8 @@ const Home = () => {
   return (
     <>
       {/* header - A screen height block with fixed background. */}
-      <header
-        className="relative h-screen font-pretty"
-        onScroll={handleScroll}
-        onClick={handleScroll}
-      >
-        <div 
-          className="absolute w-full h-screen bg-graywhite-99 -z-10"
-          style={{ transform: `translateY(${state.y/2}px)` }}
-        >
+      <header className="relative h-screen font-pretty" onScroll={handleScroll} onClick={handleScroll}>
+        <div className="absolute w-full h-screen bg-graywhite-99 -z-10" style={{ transform: `translateY(${state.y / 2}px)` }}>
           <SwitchingImage
             before={{
               src: onewayCompressed,
@@ -40,21 +33,13 @@ const Home = () => {
             after={{
               src: oneway,
               alt: "oneway",
-              className:
-                "object-cover w-full h-screen transition-filter duration-2000 ease-out ",
+              className: "object-cover w-full h-screen transition-filter duration-2000 ease-out ",
             }}
           />
         </div>
-        {/* Background color of nav itself is black, which prevents the white border due to the CSS blur. */}
-        <nav>
-          {/* Img acts as a background */}
-          {/* Brand div */}
-          <div className="animate-fadein">
-            <p className="absolute text-white left-1/3 top-1/3 font-gorgeous animate-pulse" style={{fontSize: "5rem"}}>
-              <a href="/">I'm Jimmy Lin.</a>
-            </p>
-          </div>
-        </nav>
+        <div className="absolute text-white transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 font-gorgeous animate-fadein whitespace-nowrap">
+          <p className="mb-8 text-6xl">I'm Jimmy Lin.</p>
+        </div>
       </header>
 
       {/* main - main content here. */}
@@ -76,22 +61,11 @@ const Home = () => {
             />
             <p className="p-5 text-sm">
               Hello world, I'm Jimmy. I have been working at{" "}
-              <a
-                href="https://www.chief.com.tw"
-                target="_blank"
-                rel="noreferrer"
-                className="underline text-graywhite-495 hover:text-white"
-              >
+              <a href="https://www.chief.com.tw" target="_blank" rel="noreferrer" className="underline text-graywhite-495 hover:text-white">
                 Chief Telecom
               </a>{" "}
-              for 4 years. I work for their cloud service department, maintaining and
-              developing products and services about{" "}
-              <a
-                href="https://www.chief.com.tw/cloud/"
-                target="_blank"
-                rel="noreferrer"
-                className="underline text-graywhite-495 hover:text-white"
-              >
+              for 4 years. I work for their cloud service department, maintaining and developing products and services about{" "}
+              <a href="https://www.chief.com.tw/cloud/" target="_blank" rel="noreferrer" className="underline text-graywhite-495 hover:text-white">
                 Chief Cloud
               </a>
               .
@@ -101,17 +75,14 @@ const Home = () => {
           <FadeInSection>
             <p className="p-5 text-4xl font-gorgeous">I love programming</p>
             <p className="p-5 text-sm">
-              I like to impress people with what I do. Creating is an amazing thing, and
-              programming gives us the ability to do so. I guess that's why I love
+              I like to impress people with what I do. Creating is an amazing thing, and programming gives us the ability to do so. I guess that's why I love
               programming so much.
             </p>
           </FadeInSection>
 
           <FadeInSection>
             <p className="p-5 text-4xl font-gorgeous">Skills</p>
-            <p className="p-5 text-sm">
-              Here are my skills, just in case you're interested :)
-            </p>
+            <p className="p-5 text-sm">Here are my skills, just in case you're interested :)</p>
           </FadeInSection>
 
           <div className="ml-5">
@@ -168,29 +139,16 @@ const Home = () => {
           <p className="p-5 text-sm"></p>
 
           <FadeInSection>
-            <p className="max-w-screen-sm p-5 mx-auto text-4xl font-gorgeous">
-              Thank you
-            </p>
+            <p className="max-w-screen-sm p-5 mx-auto text-4xl font-gorgeous">Thank you</p>
             <p className="max-w-screen-sm p-5 mx-auto text-sm">
-              I appreciate that you gave me sometime to introduce myself. Now, maybe you
-              would like to know more about me with:
+              I appreciate that you gave me sometime to introduce myself. Now, maybe you would like to know more about me with:
             </p>
 
             <p className="max-w-screen-sm p-5 mx-auto">
-              <a
-                href="https://github.com/j3ygithub"
-                target="_blank"
-                rel="noreferrer"
-                className="pr-4 underline text-graywhite-495 hover:text-white"
-              >
+              <a href="https://github.com/j3ygithub" target="_blank" rel="noreferrer" className="pr-4 underline text-graywhite-495 hover:text-white">
                 Github
               </a>
-              <a
-                href="https://www.cakeresume.com/b00502013"
-                target="_blank"
-                rel="noreferrer"
-                className="pr-4 underline text-graywhite-495 hover:text-white"
-              >
+              <a href="https://www.cakeresume.com/b00502013" target="_blank" rel="noreferrer" className="pr-4 underline text-graywhite-495 hover:text-white">
                 Cakeresume
               </a>
               <a
@@ -209,9 +167,7 @@ const Home = () => {
       </main>
 
       <footer className="flex items-center justify-center h-12 bg-graywhite-99 text-graywhite-693 footer font-pretty">
-        <div className="max-w-screen-sm mx-auto text-sm text-center">
-          © 2021 All rights reserved.
-        </div>
+        <div className="max-w-screen-sm mx-auto text-sm text-center">© 2021 All rights reserved.</div>
       </footer>
     </>
   );
