50041f19f53985699ddba9c4742b0405276d1ec0
Jimmy Lin <b00502013@gmail.com>
2021-07-17 21:01:29 +0800

    separate Header

--- src/index/App/HomePage.js
+++ src/index/App/HomePage.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import wall from "./HomePage/wall-1440x810.jpg";
-import wallCompressed from "./HomePage/wall-144x81.jpg";
+
 import avatar from "./HomePage/avatar-1440x1440.jpg";
 import avatarCompressed from "./HomePage/avatar-144x144.jpg";
 import ntu from "./HomePage/ntu-360x360.png";
@@ -9,13 +8,14 @@ import chief from "./HomePage/chief-500x300.png";
 import chiefCompressed from "./HomePage/chief-50x30.png";
 import FadeinWrapper from "./HomePage/FadeinWrapper";
 import ProgressiveImgWrapper from "./HomePage/ProgressiveImgWrapper";
-import LightFilter from "./HomePage/LightFilter";
+import Header from "./HomePage/Header"
 
 const HomePage = () => {
-  const [state, setState] = useState({ scrollY: window.scrollY, isDark: true });
+  const [isDark, setIsDark] = useState(true);
+  const [scrollY, setScrollY] = useState(window.scrollY);
 
   const handleScroll = () => {
-    setState({ ...state, scrollY: window.scrollY });
+    setScrollY(window.scrollY);
   };
   const addHandleScroll = () => {
     window.addEventListener("scroll", handleScroll);
@@ -23,6 +23,7 @@ const HomePage = () => {
   const removeHandleScroll = () => {
     window.removeEventListener("scroll", handleScroll);
   };
+
   useEffect(() => {
     addHandleScroll();
     return removeHandleScroll;
@@ -37,86 +38,9 @@ const HomePage = () => {
     refFooter.current.scrollIntoView();
   };
 
-  const toggleDarkMode = () => {
-    setState({ ...state, isDark: !state.isDark });
-  };
-
   return (
-    <div className={state.isDark ? "dark" : ""}>
-      <header className="relative h-screen font-pretty">
-        <LightFilter scrollY={state.scrollY} isDark={state.isDark} />
-        <div className="absolute w-full h-screen -z-10 bg-graywhite-99" style={{ transform: `translateY(${state.scrollY / 2}px)` }}>
-          <ProgressiveImgWrapper thumbnail={<img src={wallCompressed} alt="wallCompressed" className="object-cover w-full h-screen filter blur-2xl" />}>
-            <img src={wall} alt="wall" className="object-cover w-full h-screen ease-out transition-filter duration-2000" />
-          </ProgressiveImgWrapper>
-        </div>
-        <div className="absolute h-12 right-8 top-5">
-          <div className="flex items-center self-center animate-fadein-1000-700">
-            <div className="relative inline-block w-10 mr-2 align-middle transition duration-200 ease-in select-none">
-              <input
-                type="checkbox"
-                name="toggle"
-                id="toggle"
-                className="absolute block w-6 h-6 border-4 rounded-full appearance-none cursor-pointer focus:outline-none checked:right-0 bg-graywhite-198 checked:bg-white border-graywhite-594"
-                onClick={toggleDarkMode}
-              />
-              <label htmlFor="toggle" className="block h-6 overflow-hidden rounded-full cursor-pointer bg-graywhite-594"></label>
-            </div>
-            <label htmlFor="toggle" className="text-xl text-white">
-              {state.isDark ? <i className="bi bi-moon-fill"></i> : <i className="bi bi-sun-fill"></i>}
-            </label>
-          </div>
-        </div>
-        <div className="absolute flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-2/5 whitespace-nowrap">
-          <p className="mb-4 text-5xl text-graywhite-660 hover:text-white">
-            <span className="animate-fadein-1000-0">I</span>
-            <span className="animate-fadein-1000-50">'</span>
-            <span className="animate-fadein-1000-100">m</span>
-            <span className="animate-fadein-1000-150"> </span>
-            <span className="animate-fadein-1000-200">J</span>
-            <span className="animate-fadein-1000-250">i</span>
-            <span className="animate-fadein-1000-300">m</span>
-            <span className="animate-fadein-1000-350">m</span>
-            <span className="animate-fadein-1000-400">y</span>
-            <span className="animate-fadein-1000-450"> </span>
-            <span className="animate-fadein-1000-500">L</span>
-            <span className="animate-fadein-1000-550">i</span>
-            <span className="animate-fadein-1000-600">n</span>
-            <span className="animate-fadein-1000-650">.</span>
-          </p>
-          <p className="mb-16 text-xs text-center font-extralight text-graywhite-660 hover:text-white">
-            <span className="animate-fadein-2000-1500">Welcome to my personal page.</span>
-          </p>
-          <div className="flex">
-            <a
-              href="https://github.com/j3ygithub"
-              target="_blank"
-              rel="noreferrer"
-              className="flex items-center justify-center w-32 h-12 m-1 bg-graywhite-660 hover:bg-white animate-fadein-1000-900"
-            >
-              <span className="mr-1 text-2xl">
-                <i className="bi bi-github"></i>
-              </span>
-              Github
-            </a>
-            <button
-              onClick={scrollToFooter}
-              className="w-32 h-12 m-1 border-2 focus:outline-none text-graywhite-660 hover:text-white border-graywhite-660 hover:border-white text-md animate-fadein-1000-800"
-            >
-              Contact
-            </button>
-          </div>
-        </div>
-        <div className="absolute flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-4/5">
-          <span
-            onClick={scrollToAboutMe}
-            className="text-4xl text-graywhite-594 hover:text-white cursor-pointer animate-fadein-1000-900 transform hover:translate-y-0.5"
-          >
-            <i className="bi bi-chevron-compact-down"></i>
-          </span>
-        </div>
-      </header>
-
+    <div className={isDark ? "dark" : ""}>
+      <Header isDark={isDark} scrollY={scrollY} setIsDark={setIsDark} scrollToAboutMe={scrollToAboutMe} scrollToFooter={scrollToFooter}/>
       <main className="py-20 font-extralight text-graywhite-99 dark:text-graywhite-594 bg-graywhite-660 dark:bg-graywhite-99 font-pretty">
         <div ref={refAboutMe} className="max-w-screen-sm p-3 mx-auto">
           <FadeinWrapper>
---
+++ src/index/App/HomePage/Header.js
@@ -0,0 +1,88 @@
+import LightFilter from "./LightFilter";
+import wall from "./wall-1440x810.jpg";
+import wallCompressed from "./wall-144x81.jpg";
+import ProgressiveImgWrapper from "./ProgressiveImgWrapper";
+
+const Header = ({ scrollY, isDark, setIsDark, scrollToAboutMe, scrollToFooter }) => {
+  const toggleDarkMode = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <header className="relative h-screen font-pretty">
+      <LightFilter scrollY={scrollY} isDark={isDark} />
+      <div className="absolute w-full h-screen -z-10 bg-graywhite-99" style={{ transform: `translateY(${scrollY / 2}px)` }}>
+        <ProgressiveImgWrapper thumbnail={<img src={wallCompressed} alt="wallCompressed" className="object-cover w-full h-screen filter blur-2xl" />}>
+          <img src={wall} alt="wall" className="object-cover w-full h-screen ease-out transition-filter duration-2000" />
+        </ProgressiveImgWrapper>
+      </div>
+      <div className="absolute h-12 right-8 top-5">
+        <div className="flex items-center self-center animate-fadein-1000-700">
+          <div className="relative inline-block w-10 mr-2 align-middle transition duration-200 ease-in select-none">
+            <input
+              type="checkbox"
+              name="toggle"
+              id="toggle"
+              className="absolute block w-6 h-6 border-4 rounded-full appearance-none cursor-pointer focus:outline-none checked:right-0 bg-graywhite-198 checked:bg-white border-graywhite-594"
+              onClick={toggleDarkMode}
+            />
+            <label htmlFor="toggle" className="block h-6 overflow-hidden rounded-full cursor-pointer bg-graywhite-594"></label>
+          </div>
+          <label htmlFor="toggle" className="text-xl text-white">
+            {isDark ? <i className="bi bi-moon-fill"></i> : <i className="bi bi-sun-fill"></i>}
+          </label>
+        </div>
+      </div>
+      <div className="absolute flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-2/5 whitespace-nowrap">
+        <p className="mb-4 text-5xl text-graywhite-660 hover:text-white">
+          <span className="animate-fadein-1000-0">I</span>
+          <span className="animate-fadein-1000-50">'</span>
+          <span className="animate-fadein-1000-100">m</span>
+          <span className="animate-fadein-1000-150"> </span>
+          <span className="animate-fadein-1000-200">J</span>
+          <span className="animate-fadein-1000-250">i</span>
+          <span className="animate-fadein-1000-300">m</span>
+          <span className="animate-fadein-1000-350">m</span>
+          <span className="animate-fadein-1000-400">y</span>
+          <span className="animate-fadein-1000-450"> </span>
+          <span className="animate-fadein-1000-500">L</span>
+          <span className="animate-fadein-1000-550">i</span>
+          <span className="animate-fadein-1000-600">n</span>
+          <span className="animate-fadein-1000-650">.</span>
+        </p>
+        <p className="mb-16 text-xs text-center font-extralight text-graywhite-660 hover:text-white">
+          <span className="animate-fadein-2000-1500">Welcome to my personal page.</span>
+        </p>
+        <div className="flex">
+          <a
+            href="https://github.com/j3ygithub"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center justify-center w-32 h-12 m-1 bg-graywhite-660 hover:bg-white animate-fadein-1000-900"
+          >
+            <span className="mr-1 text-2xl">
+              <i className="bi bi-github"></i>
+            </span>
+            Github
+          </a>
+          <button
+            onClick={scrollToFooter}
+            className="w-32 h-12 m-1 border-2 focus:outline-none text-graywhite-660 hover:text-white border-graywhite-660 hover:border-white text-md animate-fadein-1000-800"
+          >
+            Contact
+          </button>
+        </div>
+      </div>
+      <div className="absolute flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-4/5">
+        <span
+          onClick={scrollToAboutMe}
+          className="text-4xl text-graywhite-594 hover:text-white cursor-pointer animate-fadein-1000-900 transform hover:translate-y-0.5"
+        >
+          <i className="bi bi-chevron-compact-down"></i>
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
