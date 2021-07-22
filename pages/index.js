91172147d0900eeb1ed05dd4f66dc99ca581cffc
Jimmy Lin <b00502013@gmail.com>
2021-07-23 05:14:54 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,55 @@
-import Head from 'next/head'
+import { useState, useEffect, useRef } from "react";
+import Head from "next/head";
 
-export default function Home() {
+import Header from "./HomePage/Header";
+import Main from "./HomePage/Main";
+import Footer from "./HomePage/Footer";
+
+const HomePage = () => {
+  const [isDark, setIsDark] = useState(true);
+  const [windowScrollY, setWindowScrollY] = useState(0);
+  const handleWindowScroll = () => {
+    setWindowScrollY(window.scrollY);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleWindowScroll);
+    return () => {
+      window.removeEventListener("scroll", handleWindowScroll);
+    };
+  }, []);
+  const refMain = useRef(null);
+  const scrollIntoMain = () => {
+    refMain.current.scrollIntoView();
+  };
+  const refFooter = useRef(null);
+  const scrollIntoFooter = () => {
+    refFooter.current.scrollIntoView();
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <section id="HomePage" className={isDark ? "dark" : ""}>
       <Head>
-        <title>Create Next App</title>
+        <meta charset="utf-8" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Jimmy Lin</title>
+        <meta name="title" content="I'm Jimmy Lin." />
+        <meta name="description" content="Welcome to my personal website." />
+        <meta property="og:title" content="I'm Jimmy Lin." />
+        <meta property="og:description" content="Welcome to my personal website." />
+        <meta property="og:image" content="https://jimmylin.org/avatar-1440x1440.jpg" />
       </Head>
+      <Header
+        isDark={isDark}
+        setIsDark={setIsDark}
+        windowScrollY={windowScrollY}
+        scrollIntoMain={scrollIntoMain}
+        scrollIntoFooter={scrollIntoFooter}
+      />
+      <Main refMain={refMain} />
+      <Footer refFooter={refFooter} />
+    </section>
+  );
+};
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js! I am Jimmy Lin.
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default HomePage;
