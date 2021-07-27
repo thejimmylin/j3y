06d65e6c17d1cfe3fe5980aefa74e5bde144c4df
Jimmy Lin <b00502013@gmail.com>
2021-07-27 18:26:43 +0800

    Update Main.js

--- components/HomePage/Main.js
+++ components/HomePage/Main.js
@@ -31,8 +31,8 @@ const Main = ({ refMain }) => {
         <FadeinWrapper>
           <p className="p-5 text-4xl font-medium">Hello world</p>
           <p className="p-5 text-md">
-            I'm Jimmy. I love programming. I like to create things and
-            programming gives me the chance to do so.
+            I'm Jimmy. I love programming. I like to create things and programming gives
+            me the chance to do so.
           </p>
         </FadeinWrapper>
 
@@ -90,8 +90,8 @@ const Main = ({ refMain }) => {
                 <span className="text-xl whitespace-nowrap">(2017/06 ~ )</span>
               </p>
               <p className="ml-8">
-                I work for their cloud service department. I maintain and
-                develop products and services about{" "}
+                I work for their cloud service department. I maintain and develop
+                products and services about{" "}
                 <a
                   href="https://www.chief.com.tw/cloud/"
                   target="_blank"
@@ -154,9 +154,9 @@ const Main = ({ refMain }) => {
             >
               Vue.js
             </a>
-            , containing several useful applications, such as Diary, Reminder,
-            Telecom, News, Archive, etc. It also has its own log system. It is
-            mainly used by engineers in the technical department of the company.
+            , containing several useful applications, such as Diary, Reminder, Telecom,
+            News, Archive, etc. It also has its own log system. It is mainly used by
+            engineers in the technical department of the company.
           </p>
           <p className="p-5 ml-16">
             Features:
@@ -224,15 +224,24 @@ const Main = ({ refMain }) => {
               className="underline hover:text-graywhite-330 dark:hover:text-graywhite-396"
             >
               Forti
-            </a>{" "}REST API{" "}
-            which is a multi-tenant portal for Fortigate/Fortios. With{" "}
-            FortiAdmin, It is possible to set their own config without interfere
-            with each other for different users.
+            </a>{" "}
+            REST API which is a multi-tenant portal for Fortigate/Fortios. With{" "}
+            FortiAdmin, It is possible to set their own config without interfere with
+            each other for different users.
           </p>
           <p className="p-5 ml-16">
             Features:
             <br />
-            Multi-tenant / Forti REST API
+            Multi-tenant /{" "}
+            <a
+              href="https://www.fortinet.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="underline hover:text-graywhite-330 dark:hover:text-graywhite-396"
+            >
+              Forti
+            </a>{" "}
+            REST API{" "}
           </p>
           <p className="p-5 ml-16">
             Tech stack:
@@ -262,23 +271,29 @@ const Main = ({ refMain }) => {
               className="underline hover:text-graywhite-330 dark:hover:text-graywhite-396"
             >
               Forti
-            </a>{" "}REST API{" "}
+            </a>{" "}
+            REST API{" "}
           </p>
         </FadeinWrapper>
 
         <FadeinWrapper>
           <p className="p-5 text-4xl font-medium">Skills</p>
-          <p className="p-5 text-md">
-            Here are my skills and the techniques I use:
-          </p>
+          <p className="p-5 text-md">Here are my skills and the techniques I use:</p>
         </FadeinWrapper>
 
         <FadeinWrapper>
           <p className="p-5 ml-8 text-2xl font-normal">Python</p>
           <p className="p-5 ml-8 text-md">
-            Python is my primary programming language. I am familiar with
-            Python's language features and many of its standard libraries such
-            as{" "}
+            <a
+              href="https://www.python.org/"
+              target="_blank"
+              rel="noreferrer"
+              className="underline hover:text-graywhite-330 dark:hover:text-graywhite-396"
+            >
+              Python
+            </a>{" "}
+            is my primary programming language. I am familiar with Python's language
+            features and many of its standard libraries such as{" "}
             <a
               href="https://docs.python.org/3/library/unittest.html"
               target="_blank"
@@ -330,8 +345,6 @@ const Main = ({ refMain }) => {
         <FadeinWrapper>
           <p className="p-5 ml-8 text-2xl font-normal">Django</p>
           <p className="p-5 ml-8 text-md">
-            I've spent most of my time doing web development, so I'm
-            particularly familiar with Python's web framework,{" "}
             <a
               href="https://www.djangoproject.com/"
               target="_blank"
@@ -339,9 +352,10 @@ const Main = ({ refMain }) => {
               className="underline hover:text-graywhite-330 dark:hover:text-graywhite-396"
             >
               Django
-            </a>
-            . I've been doing web development with Django for over 5 years and
-            can build modern, well-tested web application.
+            </a>{" "}
+            is a Python's web framework I've spent most of my time with. I've been doing
+            web development with Django for over 5 years. I can build modern,
+            well-tested web application with it.
           </p>
         </FadeinWrapper>
 
@@ -357,26 +371,26 @@ const Main = ({ refMain }) => {
             >
               Django REST Framework
             </a>
-            , it gets even better. Modern web applications often require REST
-            APIs, and this is where the Django REST Framework comes in.
+            , it gets even better. Modern web applications often require REST APIs, and
+            this is where the Django REST Framework comes in.
           </p>
         </FadeinWrapper>
 
         <FadeinWrapper>
           <p className="p-5 ml-8 text-2xl font-normal">SQL</p>
           <p className="p-5 ml-8 text-md">
-            Django has a great model/ORM system, which makes it possible to
-            creating a web application without writing SQL. But sometimes SQL
-            knowledge is important, too. It is true especially when it comes to
-            a extremely complicated queriy or some performance issue.
+            Django has a great model/ORM system, which makes it possible to creating a
+            web application without writing SQL. But sometimes SQL knowledge is
+            important, too. It is true especially when it comes to a extremely
+            complicated queriy or some performance issue.
           </p>
         </FadeinWrapper>
 
         <FadeinWrapper>
           <p className="p-5 ml-8 text-2xl font-normal">React.js</p>
           <p className="p-5 ml-8 text-md">
-            Django is a backend framework I use a lot, and when it comes to
-            frontend things,{" "}
+            Django is a backend framework I use a lot, and when it comes to frontend
+            things,{" "}
             <a
               href="https://reactjs.org/"
               target="_blank"
@@ -385,9 +399,9 @@ const Main = ({ refMain }) => {
             >
               React.js
             </a>{" "}
-            is my favorite because its philosophy is so graceful, making you
-            enjoy coding and building things with it. This website is built with
-            React.js, too.
+            is my favorite because its philosophy is so graceful, making you enjoy
+            coding and building things with it. This website is built with React.js,
+            too.
           </p>
         </FadeinWrapper>
 
@@ -403,8 +417,8 @@ const Main = ({ refMain }) => {
             >
               Vue.js
             </a>
-            . I personally like React.js more, but they are both very powerful
-            andd reliable frameworks.
+            . I personally like React.js more, but they are both very powerful andd
+            reliable frameworks.
           </p>
         </FadeinWrapper>
 
@@ -428,18 +442,17 @@ const Main = ({ refMain }) => {
             >
               Bootstrap
             </a>
-            , but it is much more flexible and easy to customize, which prevents
-            you from building a lot of similar web.
+            , but it is much more flexible and easy to customize, which prevents you
+            from building a lot of similar web.
           </p>
         </FadeinWrapper>
 
         <FadeinWrapper>
           <p className="p-5 ml-8 text-2xl font-normal">HTML/CSS/JavaScript</p>
           <p className="p-5 ml-8 text-md">
-            In addition to developing with frontend frameworks, I also know
-            about HTML/CSS/JavaScript. I think, after all, frameworks are based
-            on these three elements, so it is important to have a solid
-            understanding of them.
+            In addition to developing with frontend frameworks, I also know about
+            HTML/CSS/JavaScript. I think, after all, frameworks are based on these three
+            elements, so it is important to have a solid understanding of them.
           </p>
         </FadeinWrapper>
 
@@ -455,9 +468,9 @@ const Main = ({ refMain }) => {
             >
               Github action
             </a>{" "}
-            to handle CI/CD things. I believe that deployment is just as
-            important as development. If a feature can't be delivered to the
-            users, the resources spent on development will be wasted.
+            to handle CI/CD things. I believe that deployment is just as important as
+            development. If a feature can't be delivered to the users, the resources
+            spent on development will be wasted.
           </p>
         </FadeinWrapper>
 
@@ -473,9 +486,8 @@ const Main = ({ refMain }) => {
             >
               Docker
             </a>
-            , which frees me from a lot of environment and compatibility
-            concerns. Docker greatly increases the portability and flexibility
-            of the services.
+            , which frees me from a lot of environment and compatibility concerns.
+            Docker greatly increases the portability and flexibility of the services.
           </p>
         </FadeinWrapper>
 
@@ -490,8 +502,8 @@ const Main = ({ refMain }) => {
             >
               GCP
             </a>{" "}
-            is where most of my services are hosted. This website is such a
-            example. I usually use their compute engines with Docker.
+            is where most of my services are hosted. This website is such a example. I
+            usually use their compute engines with Docker.
           </p>
         </FadeinWrapper>
 
@@ -561,8 +573,8 @@ const Main = ({ refMain }) => {
         <FadeinWrapper>
           <p className="p-5 text-4xl font-medium">Thank you</p>
           <p className="p-5 text-md">
-            thank you for giving a chance to introduce myself. I really
-            apprecicate that. Feel free to contact me if you want.
+            thank you for giving a chance to introduce myself. I really apprecicate
+            that. Feel free to contact me if you want.
           </p>
         </FadeinWrapper>
       </div>
