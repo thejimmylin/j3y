0c5bff267c4620e06dcfb92fd244d650a0a885c1
Jimmy Lin <b00502013@gmail.com>
2021-07-31 20:55:57 +0800

    stop using mdx and typescript

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,8 @@
-import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
-import { MDXProvider } from "@mdx-js/react";
-
-const components = {
-  p: (props) => <p className="m-4" {...props} />,
-  h1: (props) => <h1 className="m-4 text-4xl font-medium" {...props} />,
-  h2: (props) => <h2 className="m-4 text-3xl font-medium" {...props} />,
-  h3: (props) => <h3 className="m-4 text-2xl font-medium" {...props} />,
-  h4: (props) => <h4 className="m-4 text-xl font-medium" {...props} />,
-  h5: (props) => <h5 className="m-4 text-lg font-medium" {...props} />,
-  h6: (props) => <h6 className="m-4 font-medium text-md" {...props} />,
-  ol: (props) => <ol className="m-4 list-decimal" {...props} />,
-  li: (props) => <li className="m-4" {...props} />,
-  inlineCode: (props) => <code className="text-white bg-gray-700" {...props} />,
-  code: (props) => (
-    <code
-      className="block p-4 m-4 overflow-x-scroll text-white whitespace-pre bg-gray-700"
-      {...props}
-    />
-  ),
-};
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <MDXProvider components={components}>
-      <Component {...pageProps} />
-    </MDXProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
