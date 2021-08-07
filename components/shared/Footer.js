caaeaa2630af2ffbf1d11a9b9cce88c534e89546
Jimmy Lin <b00502013@gmail.com>
2021-08-07 14:54:43 +0800

    Update Footer.js

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -8,22 +8,22 @@ const Footer = ({ extraClassNames }) => {
         extraClassNames
       )}
     >
-      <div className="flex flex-col flex-wrap items-end content-start max-w-screen-sm p-12 mx-auto">
-        <p className="pb-4 text-5xl font-medium">Jimmy Lin</p>
-        <p className="pb-8">
+      <div className="max-w-screen-sm mx-auto flex flex-col flex-wrap items-end content-start p-4">
+        <p className="text-5xl font-medium hover:text-black dark:hover:text-white mb-4 cursor-default">Jimmy Lin</p>
+        <p className="mb-4">
           <a
             href="mailto:contact@jimmylin.org"
-            className="underline text-pencil hover:text-ink dark:text-moonlight dark:hover:text-light"
+            className="underline hover:text-black dark:hover:text-white"
           >
             contact@jimmylin.org
           </a>
         </p>
-        <p className="-mr-4">
+        <p className="text-ink dark:text-light text-4xl mb-4 flex gap-2">
           <a
             href="https://github.com/j3ygithub"
             target="_blank"
             rel="noreferrer"
-            className="pr-4 text-4xl underline text-pencil hover:text-ink dark:text-moonlight dark:hover:text-light"
+            className="transform hover:scale-110"
           >
             <i className="bi bi-github"></i>
           </a>
@@ -31,7 +31,7 @@ const Footer = ({ extraClassNames }) => {
             href="https://www.linkedin.com/in/jimmy-lin-5779a61b5/"
             target="_blank"
             rel="noreferrer"
-            className="pr-4 text-4xl underline text-pencil hover:text-ink dark:text-moonlight dark:hover:text-light"
+            className="transform hover:scale-110"
           >
             <i className="bi bi-linkedin"></i>
           </a>
@@ -39,7 +39,7 @@ const Footer = ({ extraClassNames }) => {
             href="https://www.facebook.com/j3y.fb"
             target="_blank"
             rel="noreferrer"
-            className="pr-4 text-4xl underline text-pencil hover:text-ink dark:text-moonlight dark:hover:text-light"
+            className="transform hover:scale-110"
           >
             <i className="bi bi-facebook"></i>
           </a>
