6c8586622ffc7dbed02c213fb490c481ddcd9b5b
Jimmy Lin <b00502013@gmail.com>
2021-08-30 14:40:38 +0800

    crossOrigin="true"

--- pages/_app.js
+++ pages/_app.js
@@ -44,7 +44,7 @@ const App = ({ Component, pageProps }) => {
           content="https://jimmylin.org/avatar-1440x1440.jpg"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap"
           rel="stylesheet"
