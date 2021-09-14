68c47a434ac4e9d0fda55e579aaaae82faf64829
Jimmy Lin <b00502013@gmail.com>
2021-09-14 16:52:48 +0800

    update api routes

--- prisma/client.js
+++ prisma/client.js
@@ -1,9 +1,9 @@
 // Use some tricks to prevent Next.js from creating too many `PrismaClient` instances in development due to hot reloading. To learn more:
 // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#solution
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prismaClient;
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prismaClient = new PrismaClient();
 } else {
   if (!global.prismaClient) {
