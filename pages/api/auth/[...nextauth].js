11076c6b2c2c6a8a10549cb840995136fd7020c7
Jimmy Lin <b00502013@gmail.com>
2021-09-21 20:01:18 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,8 +8,15 @@ export default NextAuth({
   adapter: PrismaAdapter(prismaClient),
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
     }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
