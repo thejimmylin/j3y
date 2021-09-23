ba21b5bd31d9cfd68e124ca026e1c1bebe417af5
Jimmy Lin <b00502013@gmail.com>
2021-09-23 23:48:47 +0800

    Update layout

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -2,7 +2,10 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <main className="font-sans text-ink bg-paper dark:text-light dark:bg-night transition-bg min-h-screen py-20">
-        <div className="max-w-screen-md mx-auto">{children}</div>
+        <div className="max-w-screen-lg mx-auto">
+          <div className="max-w-screen-md">{children}</div>
+          <div></div>
+        </div>
       </main>
     </>
   );
