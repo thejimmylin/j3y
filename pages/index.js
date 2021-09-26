31f5bed95f02e4519c92f58fb12e99d79f33a8dc
Jimmy Lin <b00502013@gmail.com>
2021-09-26 17:05:16 +0800

    fix about-me

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 import MainLayout from "../components/MainLayout";
 import PostLayout from "../components/PostLayout";
 import Footer from "../components/Footer";
-import useMDXComponent from "../hooks/useMDXComponents";
+import AboutMe from "../components/AboutMe";
 
 const HomePage = ({ useIsDark }) => {
   const [windowScrollY] = useWindowScrollY();
@@ -19,7 +19,9 @@ const HomePage = ({ useIsDark }) => {
       />
       <Header useIsDark={useIsDark} headerRef={headerRef} />
       <MainLayout>
-        <PostLayout></PostLayout>
+        <PostLayout>
+          <AboutMe />
+        </PostLayout>
       </MainLayout>
       <Footer />
     </>
