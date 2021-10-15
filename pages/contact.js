4241e9ddb40619b630bb38d8448adbfbea27e8e7
Jimmy Lin <b00502013@gmail.com>
2021-10-15 16:59:54 +0800

    add `onSubmit` and preventDefault.

--- pages/contact.js
+++ pages/contact.js
@@ -2,39 +2,43 @@ import Header from "../components/Header";
 import MainLayout from "../components/MainLayout";
 import PostLayout from "../components/PostLayout";
 import Footer from "../components/Footer";
-import { H2, P } from "../components/markdown-components";
+import { H1, P } from "../components/markdown-components";
 
 const HomePage = ({ useIsDark }) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <>
       <Header useIsDark={useIsDark} />
       <MainLayout>
         <PostLayout>
-          <H2>Contact me</H2>
+          <H1>Contact me</H1>
           <P>
-            You can leave me some messages here, about jobs, cooperation, some
-            advices or anything else you want to tell me. I would contact you
-            with the Email you provide.
+            You can leave me some messages here. I would reply it with Email you
+            leave.
           </P>
           <div className="mt-8">
-            <form>
+            <form noValidate onSubmit={onSubmit}>
               <div className="flex flex-col gap-4">
                 <input
                   type="email"
-                  placeholder="Email"
-                  className="text-pencil bg-paper-light dark:text-moonlight dark:bg-night-light placeholder-current outline-none py-2 px-4"
+                  placeholder="Your Email"
+                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light placeholder-current outline-none py-2 px-4 transition-bg"
                 />
                 <textarea
                   type="text"
                   rows="4"
                   placeholder="Message"
-                  className="text-pencil bg-paper-light dark:text-moonlight dark:bg-night-light placeholder-current outline-none py-2 px-4"
+                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light placeholder-current outline-none py-2 px-4 transition-bg"
                 />
                 <button
                   type="submit"
-                  className="text-ink bg-paper-light dark:text-light dark:bg-night-light py-2 px-4"
+                  className="text-ink bg-paper-light dark:text-light dark:bg-night-light py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800 transition-bg"
                 >
-                  Submit
+                  <span className="text-lg font-semibold">Submit</span>
                 </button>
               </div>
             </form>
