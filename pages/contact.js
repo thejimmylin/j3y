887268c5eeb24e1c22d1ea1d316b8cf483f2c523
Jimmy Lin <b00502013@gmail.com>
2021-10-17 01:37:45 +0800

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import classNames from "classnames";
 import Header from "../components/Header";
 import MainLayout from "../components/MainLayout";
 import PostLayout from "../components/PostLayout";
@@ -7,50 +6,34 @@ import Footer from "../components/Footer";
 import { H1, P } from "../components/markdown-components";
 
 const Contact = ({ useIsDark }) => {
-
+  const [isEmailDone, setIsEmailDone] = useState(false);
   const [email, setEmail] = useState("");
   const getEmailError = () => {
-    if (!email) {
-      return "Please provide an email.";
-    }
-    if (!(email.includes("@") && email.includes("."))) {
-      return "Please provide an valid email";
-    }
+    if (isEmailDone && !email) return "Please provide an email.";
+    if (email && !(email.includes("@") && email.includes(".")))
+      return "Please provide an valid email.";
     return "";
-  };
-  const getEmailClassName = () => {
-    const isValid = email && email.includes("@") && email.includes(".");
-    return classNames({
-      "placeholder-moonlight dark:placeholder-pencil bg-paper-light dark:bg-night-light transition-bg outline-none w-full py-2 px-4": true,
-      "text-ink dark:text-light": isValid,
-      "text-red-500 border border-red-500": !isValid,
-    });
   };
   const emailOnChange = (e) => {
     setEmail(e.target.value);
+    setIsEmailDone(false);
   };
 
+  const [isMessageDone, setIsMessageDone] = useState(false);
   const [message, setMessage] = useState("");
   const getMessageError = () => {
-    if (!message) {
-      return "Please provide a message.";
-    }
+    if (isMessageDone && !message) return "Please provide a message.";
+    if (!message) return "Please provide a message.";
     return "";
-  };
-  const getMessageClassName = () => {
-    const isValid = message && message.includes("@") && message.includes(".");
-    return classNames({
-      "placeholder-moonlight dark:placeholder-pencil bg-paper-light dark:bg-night-light transition-bg outline-none w-full py-2 px-4": true,
-      "text-ink dark:text-light": isValid,
-      "text-red-500 border border-red-500": isValid,
-    });
   };
   const messageOnChange = (e) => {
     setMessage(e.target.value);
+    setIsMessageDone(false);
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
+    setIsEmailDone(true);
     console.log(
       `Sending a Email to ${email} with below message:\n\n${message}`
     );
@@ -70,7 +53,7 @@ const Contact = ({ useIsDark }) => {
                   <input
                     type="email"
                     placeholder="Email"
-                    className={getEmailClassName()}
+                    className="placeholder-moonlight dark:placeholder-pencil bg-paper-light dark:bg-night-light transition-bg outline-none w-full py-2 px-4"
                     value={email}
                     onChange={emailOnChange}
                   />
@@ -86,7 +69,7 @@ const Contact = ({ useIsDark }) => {
                   <textarea
                     rows="4"
                     placeholder="Message"
-                    className={getMessageClassName()}
+                    className="placeholder-moonlight dark:placeholder-pencil bg-paper-light dark:bg-night-light transition-bg outline-none w-full py-2 px-4"
                     value={message}
                     onChange={messageOnChange}
                   />
