be5de2f9ac045861768b0631e379a06516377d5e
Jimmy Lin <b00502013@gmail.com>
2021-10-16 14:15:35 +0800

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import MainLayout from "../components/MainLayout";
 import PostLayout from "../components/PostLayout";
@@ -5,40 +6,48 @@ import Footer from "../components/Footer";
 import { H1, P } from "../components/markdown-components";
 
 const HomePage = ({ useIsDark }) => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const emailOnChange = (e) => {
+    setEmail(e.target.value);
+
+  };
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("submit");
+    console.log(
+      `Sending a Email to ${email} with below message:\n\n${message}`
+    );
   };
-
   return (
     <>
       <Header useIsDark={useIsDark} />
       <MainLayout>
         <PostLayout>
           <H1>Contact me</H1>
-          <P>
-            You can leave me some messages here. I would reply it with Email you
-            leave.
-          </P>
+          <P>You can leave me a message here. I would reply you with email.</P>
           <div className="mt-8">
             <form noValidate onSubmit={onSubmit}>
               <div className="flex flex-col gap-4">
                 <input
                   type="email"
                   placeholder="Your Email"
-                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light placeholder-current outline-none py-2 px-4 transition-bg"
+                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light outline-none py-2 px-4 placeholder-current transition-bg"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <textarea
                   type="text"
                   rows="4"
                   placeholder="Message"
-                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light placeholder-current outline-none py-2 px-4 transition-bg"
+                  className="text-moonlight bg-paper-light dark:text-pencil dark:bg-night-light outline-none py-2 px-4 placeholder-current transition-bg"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 />
                 <button
                   type="submit"
-                  className="text-ink bg-paper-light dark:text-light dark:bg-night-light py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800 transition-bg"
+                  className="text-ink bg-paper-light dark:text-light dark:bg-night-light outline-none py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-800 transition-bg"
                 >
-                  <span className="text-lg font-semibold">Submit</span>
+                  Submit
                 </button>
               </div>
             </form>
