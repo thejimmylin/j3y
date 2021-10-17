994d484e05431616d67547991df02b85d2d0216e
Jimmy Lin <b00502013@gmail.com>
2021-10-17 23:07:53 +0800

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,7 +2,6 @@ import nodemailer from "nodemailer";
 
 const NODEMAILER_AUTH_USER = process.env.NODEMAILER_AUTH_USER;
 const NODEMAILER_AUTH_PASS = process.env.NODEMAILER_AUTH_PASS;
-const FROM = "noreply@jimmy.org";
 const TO = "b00502013@gmail.com";
 
 const transporter = nodemailer.createTransport({
@@ -16,28 +15,28 @@ const transporter = nodemailer.createTransport({
 });
 
 const handler = async (req, res) => {
-  const mailOptions = {
-    from: FROM,
-    to: TO,
-    subject: `Sent by ${req.body.email} at ${new Date()}.`,
-    text: req.body.message,
-  };
-
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
+  if (req.method === "POST") {
+    const mailOptions = {
+      to: TO,
+      subject: `Sent by ${req.body.email} at ${new Date()}.`,
+      text: req.body.message,
+    };
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
     });
-  });
-
-  return res.status(200).json({
-    msg: `Sending a E-mail to ${TO} with nodemailer using.`,
-  });
+    return res.status(200).json({
+      msg: `Sending a E-mail to ${TO} with nodemailer using.`,
+    });
+  }
+  return res.status(405).end();
 };
 
 export default handler;
